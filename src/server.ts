22946c621144beef0ec97d000b1e12d971770dbe
Desson Medeiros <dessonwms@gmail.com>
2021-08-18 16:30:45 -0400

    chore: add eslint eslint-import-resolver-typescript

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import express = require('express')
+import express from 'express';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(routes);
 
-app.listen(3333, function () {
-  console.log('Server started in port 3333!')
-})
+app.listen(3333, () => {
+  console.log('Server started in port 3333!');
+});
