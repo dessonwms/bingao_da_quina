20234511600a199ff4a04ef2e03f9d097819ec33
Desson Medeiros <dessonwms@gmail.com>
2021-08-21 10:58:03 -0400

    feat: screen signIn Home

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = Router();
 
 routes.get('/', (request, response) =>
-  response.json({ message: 'Hello World' }),
+  response.render('index', { name: 'Desson' }),
 );
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import nunjucks from 'nunjucks';
 import routes from './routes';
 
 const app = express();
+
+app.use(express.static('src/public'));
+
+nunjucks.configure('src/views', {
+  express: app,
+});
+
+app.set('view engine', 'njk');
 
 app.use(routes);
 
