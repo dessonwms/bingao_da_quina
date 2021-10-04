578f5ebf395345e8689abf94c704c9f6ac91e500
Desson Medeiros <dessonwms@gmail.com>
2021-10-03 20:14:08 -0400

    test: download pdf home puppeteer goto 8

--- src/app/controllers/QuinaryController.ts
+++ src/app/controllers/QuinaryController.ts
@@ -1,9 +1,17 @@
 /* eslint-disable camelcase */
+import nunjucks from 'nunjucks';
+import puppeteer from 'puppeteer';
+import path from 'path';
+
 import QuinaryModel from '../models/Quinary';
 import BetsModel from '../models/Bettings';
 import BingoModel from '../models/Bingo';
+import ReportModel from '../models/Report';
+
+import globalInfo from '../../config/globalInfo';
 import format from '../../lib/format';
 import verifyWinner from '../../lib/verifyWinner';
+import summaryValues from '../../lib/summaryValues';
 
 import SaveWinnerInformationInTheWinnersTableService from '../services/SaveWinnerInformationInTheWinnersTableService';
 
@@ -71,6 +79,148 @@ const PunterController = {
         bettings,
         noDuplicates,
         bingo.id,
+      );
+
+      // GERA E FORMATA DADOS PARA EXIBIÇÃO NO PDF
+      // Retorna lista com todas as 80 dezenas da Quina
+      results = await QuinaryModel.allTen();
+      const quinaryTens = results.rows;
+      // Retorna o total de apostas registradas no sistema
+      const totalBets = await ReportModel.totalRegister({
+        table: 'bettings',
+        bingoId: bingo.id,
+        limit: 1,
+      });
+      // Retorna os dados de ranking de acertos
+      results = await ReportModel.rankingHome(bingo.id);
+      const ratings = results.rows;
+
+      // Cálculos de resumo do bingo
+      const summary = {
+        totalBets: totalBets.count,
+        valueBets: format.formatPrice(summaryValues.valueBet),
+        grandTotal: format.formatPrice(
+          summaryValues.grandTotal(totalBets.count),
+        ),
+        administrationFee: format.formatPrice(
+          summaryValues.administrationFee(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        firstPlaceAward: format.formatPrice(
+          summaryValues.firstPlaceAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        secondPlaceAward: format.formatPrice(
+          summaryValues.secondPlaceAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        minorHitAward: format.formatPrice(
+          summaryValues.minorHitAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        prizeTotal: format.formatPrice(
+          summaryValues.prizeTotal(summaryValues.grandTotal(totalBets.count)),
+        ),
+        prizeForTenHits: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            10,
+            summaryValues.firstPlaceAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+        prizeForNineHits: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            9,
+            summaryValues.secondPlaceAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+        prizeMinorHit: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            ratings[0]?.minimo,
+            summaryValues.minorHitAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+      };
+
+      // Retorna lista de todas as apostas referentes a edição atual do Bingo
+      results = await BetsModel.summaryPdf(bingo.id);
+      const bettingsPdf = results.rows;
+
+      // TRATA DADOS PARA EXIBIÇÃO NA TABELA DE RANKING
+      const dataSummary = await summaryValues.generateRankingData(ratings);
+
+      // GERA PDF
+      nunjucks.render(
+        'bingo/summary_pdf/index.njk',
+        {
+          totalRatings: dataSummary.length,
+          ratings,
+          summary,
+          bingo,
+          quinarys,
+          quinaryTens,
+          noDuplicates,
+          globalInfo,
+          dataSummary,
+          bettings: bettingsPdf,
+        },
+        // eslint-disable-next-line consistent-return
+        async (error: any, html: any) => {
+          if (error) {
+            console.log(`Erro: ${error}`);
+          } else {
+            try {
+              // Caminho para salvar o arquivo
+              const pathPdf = path.resolve(__dirname, '..', '..', '..', 'temp');
+
+              // Nome do arquivo PDF
+              const fileName = `bingao_da_quina_edicao_${bingo.edition}`;
+
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+
+              await page.setContent(html);
+
+              // eslint-disable-next-line no-unused-vars
+              const pdf = await page.pdf({
+                printBackground: true,
+                format: 'a4',
+                landscape: false,
+                path: `${pathPdf}/${fileName}.pdf`,
+                margin: {
+                  top: '20px',
+                  bottom: '20px',
+                  left: '20px',
+                  right: '20px',
+                },
+              });
+
+              await browser.close();
+
+              // Adiciona o nome do arquivo na tabela de bingos
+              await BingoModel.update(bingo.id, {
+                summary_pdf: `${fileName}.pdf`,
+              });
+            } catch (err) {
+              console.log(err);
+              return response.render('quinary/register', {
+                error: 'Algum erro aconteceu',
+              });
+            }
+          }
+        },
       );
 
       // Verifica se houve ganhador no bingo
@@ -179,6 +329,150 @@ const PunterController = {
         bettings,
         noDuplicates,
         bingo.id,
+      );
+
+      // GERA E FORMATA DADOS PARA EXIBIÇÃO NO PDF
+      // Retorna lista com todas as 80 dezenas da Quina
+      results = await QuinaryModel.allTen();
+      const quinaryTens = results.rows;
+
+      // Retorna o total de apostas registradas no sistema
+      const totalBets = await ReportModel.totalRegister({
+        table: 'bettings',
+        bingoId: bingo.id,
+        limit: 1,
+      });
+
+      // Retorna os dados de ranking de acertos
+      results = await ReportModel.rankingHome(bingo.id);
+      const ratings = results.rows;
+
+      // Cálculos de resumo do bingo
+      const summary = {
+        totalBets: totalBets.count,
+        valueBets: format.formatPrice(summaryValues.valueBet),
+        grandTotal: format.formatPrice(
+          summaryValues.grandTotal(totalBets.count),
+        ),
+        administrationFee: format.formatPrice(
+          summaryValues.administrationFee(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        firstPlaceAward: format.formatPrice(
+          summaryValues.firstPlaceAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        secondPlaceAward: format.formatPrice(
+          summaryValues.secondPlaceAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        minorHitAward: format.formatPrice(
+          summaryValues.minorHitAward(
+            summaryValues.grandTotal(totalBets.count),
+          ),
+        ),
+        prizeTotal: format.formatPrice(
+          summaryValues.prizeTotal(summaryValues.grandTotal(totalBets.count)),
+        ),
+        prizeForTenHits: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            10,
+            summaryValues.firstPlaceAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+        prizeForNineHits: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            9,
+            summaryValues.secondPlaceAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+        prizeMinorHit: format.formatPrice(
+          summaryValues.prizePerWinner(
+            ratings,
+            ratings[0]?.minimo,
+            summaryValues.minorHitAward(
+              summaryValues.grandTotal(totalBets.count),
+            ),
+          ),
+        ),
+      };
+
+      // Retorna lista de todas as apostas referentes a edição atual do Bingo
+      results = await BetsModel.summaryPdf(bingo.id);
+      const bettingsPdf = results.rows;
+
+      // TRATA DADOS PARA EXIBIÇÃO NA TABELA DE RANKING
+      const dataSummary = await summaryValues.generateRankingData(ratings);
+
+      // GERA PDF
+      nunjucks.render(
+        'bingo/summary_pdf/index.njk',
+        {
+          totalRatings: dataSummary.length,
+          ratings,
+          summary,
+          bingo,
+          quinarys,
+          quinaryTens,
+          noDuplicates,
+          globalInfo,
+          dataSummary,
+          bettings: bettingsPdf,
+        },
+        // eslint-disable-next-line consistent-return
+        async (error: any, html: any) => {
+          if (error) {
+            console.log(`Erro: ${error}`);
+          } else {
+            try {
+              // Caminho para salvar o arquivo
+              const pathPdf = path.join(__dirname, '..', '..', '..', 'temp');
+              // Nome do arquivo PDF
+              const fileName = `bingao_da_quina_edicao_${bingo.edition}`;
+
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+
+              await page.setContent(html);
+
+              // eslint-disable-next-line no-unused-vars
+              const pdf = await page.pdf({
+                printBackground: true,
+                format: 'a4',
+                landscape: false,
+                path: `${pathPdf}/${fileName}.pdf`,
+                margin: {
+                  top: '20px',
+                  bottom: '20px',
+                  left: '20px',
+                  right: '20px',
+                },
+              });
+
+              await browser.close();
+
+              // Adiciona o nome do arquivo na tabela de bingos
+              await BingoModel.update(bingo.id, {
+                summary_pdf: `${fileName}.pdf`,
+              });
+            } catch (err) {
+              console.log(err);
+
+              return response.render('quinary/edit', {
+                error: err,
+              });
+            }
+          }
+        },
       );
 
       // Verifica se houve ganhador no bingo
